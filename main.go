d12e558bde603f3917cc80a253170449910953aa
CarlosGutierrezArdila <chgutierreza@correo.udistrital.edu.co>
2020-03-18 21:57:12 -0500

    adjust container db connection error

--- main.go
+++ main.go
@@ -24,21 +24,29 @@ type Person struct {
 
 // func to create
 func Create(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("create")
 	response.Header().Add("content-type", "application/json") // serve in
 	var person Person
 	json.NewDecoder(request.Body).Decode(&person)
 	collection := client.Database("test").Collection("people")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	result, _ := collection.InsertOne(ctx, person)
+	result, err := collection.InsertOne(ctx, person)
+	if err != nil {
+		fmt.Println("err insert", err.Error())
+	}
 	json.NewEncoder(response).Encode(result)
 
 }
 
 func main() {
 	fmt.Println("Starting")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)                    //create context with timeout
-	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017")) //connect to the db TODO get params from conf
-	router := mux.NewRouter()                                                              // define the router
-	router.HandleFunc("/person", Create).Methods("POST")                                   // create the route
-	http.ListenAndServe(":8086", router)                                                   // define the port TODO get from conf
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	var errdb interface{}                                                                                                                                      //create context with timeout
+	client, errdb = mongo.Connect(ctx, options.Client().ApplyURI("mongodb://test:test@api_mongo_db:27017/?authSource=admin&readPreference=primary&ssl=false")) //connect to the db TODO get params from conf
+	if errdb != nil {
+		fmt.Println("cannot connect to DB")
+	}
+	router := mux.NewRouter()                            // define the router
+	router.HandleFunc("/person", Create).Methods("POST") // create the route
+	http.ListenAndServe(":8086", router)                 // define the port TODO get from conf
 }
